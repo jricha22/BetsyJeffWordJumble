2e0557455b74b646fc0bd8574275732222fe3c98
Jeff Richards <jeff.richards@earthlink.net>
2016-03-19 11:24:24 -0400

    Add iteration 1 model and tests

--- app/src/main/java/edu/betsyjeffwordjumble/model/WordJumble.java
+++ app/src/main/java/edu/betsyjeffwordjumble/model/WordJumble.java
@@ -1,6 +1,7 @@
 package edu.betsyjeffwordjumble.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -13,6 +14,7 @@ import java.util.Random;
 public class WordJumble {
     private List<String> words;
     private Random randomGenerator;
+    private String currentWord;
 
     /**
      * Default constructor.
@@ -37,10 +39,28 @@ public class WordJumble {
     }
 
     /**
-     * Get a word from the word list
-     * @return A word from the word list
+     * Get a word from the word list that is randomly scrambled.
+     * Remembers the selected word, can be checked by calling checkResult.
+     * @return A word from the word list with letters randomly shuffled
      */
     public String getAWord() {
-        return this.words.get(this.randomGenerator.nextInt(this.words.size()));
+        this.currentWord = this.words.get(this.randomGenerator.nextInt(this.words.size()));
+        List<Character> characters = new ArrayList<>();
+        for(char c :  this.currentWord.toCharArray())
+            characters.add(c);
+        Collections.shuffle(characters, this.randomGenerator);
+        StringBuilder myBuilder = new StringBuilder();
+        for(char aChar : characters)
+            myBuilder.append(aChar);
+        return myBuilder.toString();
+    }
+
+    /**
+     * Check if a guessed word is the current word.
+     * @param theGuess The word guessed
+     * @return True if word is correct, false otherwise
+     */
+    public boolean checkResult(String theGuess) {
+        return theGuess.equals(this.currentWord);
     }
 }
--- app/src/test/java/edu/westga/betsyjeffwordjumble/WhenCreateWordJumbleTests.java
+++ app/src/test/java/edu/westga/betsyjeffwordjumble/WhenCreateWordJumbleTests.java
@@ -10,9 +10,51 @@ import static org.junit.Assert.*;
  * Unit tests for WordJumble model creation
  */
 public class WhenCreateWordJumbleTests {
+    /**
+     * Test that a word is returned after the model is created
+     */
     @Test
     public void testProvidesAWordAfterCreation() {
         WordJumble myWordJumble = new WordJumble();
         assertNotNull(myWordJumble.getAWord());
+    }
+
+
+    /**
+     * Test that default word length is 5
+     */
+    @Test
+    public void testProvidesAFiveCharacterWordAfterCreation() {
+        WordJumble myWordJumble = new WordJumble();
+        assertEquals(5, myWordJumble.getAWord().length());
+    }
+
+    /**
+     * Test that returned word is scrambled
+     */
+    @Test
+    public void testReturnedWordIsScrambled() {
+        WordJumble myWordJumble = new WordJumble(100);
+        assertEquals("iuckq", myWordJumble.getAWord());
+    }
+
+    /**
+     * Test guessing correctly returns True
+     */
+    @Test
+    public void testGuessWordCorrectlyIsTrue() {
+        WordJumble myWordJumble = new WordJumble(100);
+        String jumbled = myWordJumble.getAWord();
+        assertEquals(true, myWordJumble.checkResult("quick"));
+    }
+
+    /**
+     * Test guessing incorrectly returns False
+     */
+    @Test
+    public void testGuessWordIncorrectlyIsFalse() {
+        WordJumble myWordJumble = new WordJumble(100);
+        String jumbled = myWordJumble.getAWord();
+        assertEquals(false, myWordJumble.checkResult("meter"));
     }
 }
