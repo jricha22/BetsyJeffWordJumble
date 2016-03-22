68308a57c36ab3ec47d345df465272b4ec3e87f1
Betsy Louis <blouis1@my.westga.edu>
2016-03-22 14:48:23 -0400

    Scrambled word displays as letter tiles.

--- app/src/main/java/edu/westga/betsyjeffwordjumble/controller/Controller.java
+++ app/src/main/java/edu/westga/betsyjeffwordjumble/controller/Controller.java
@@ -8,13 +8,23 @@ import edu.westga.betsyjeffwordjumble.model.WordJumble;
  */
 public class Controller {
     private WordJumble m_wordJumble;
+    private String m_theWord;
 
     public Controller() {
         m_wordJumble = new WordJumble();
     }
 
+    public String getTheWord() {
+        return m_theWord;
+    }
+
+    public void setTheWord(String theWord) {
+        m_theWord = theWord;
+    }
+
     public String getAWord() {
-        return m_wordJumble.getAWord();
+        m_theWord = m_wordJumble.getAWord();
+        return m_theWord;
     }
 
     public boolean checkResult(String theGuess) {
@@ -22,15 +32,25 @@ public class Controller {
     }
 
     public String getAFiveCharWord() {
-        return m_wordJumble.getAFiveCharWord();
+        m_theWord = m_wordJumble.getAFiveCharWord();
+        return m_theWord;
     }
 
     public String getASixCharWord() {
-        return m_wordJumble.getASixCharWord();
+        m_theWord = m_wordJumble.getASixCharWord();
+        return m_theWord;
     }
 
     public String getAHint() {
-        return m_wordJumble.getAHint();
+        return m_wordJumble.getAHint().toUpperCase();
+    }
+
+    public String getLetterAtPosition(int position) {
+        String word = m_theWord;
+        if (position > word.length() - 1) {
+            throw new IllegalArgumentException("Invalid position in word");
+        }
+        return Character.toString(word.charAt(position)).toUpperCase();
     }
 
 }
--- app/src/main/java/edu/westga/betsyjeffwordjumble/view/GameScreenActivity.java
+++ app/src/main/java/edu/westga/betsyjeffwordjumble/view/GameScreenActivity.java
@@ -1,6 +1,7 @@
 package edu.westga.betsyjeffwordjumble.view;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,16 +26,20 @@ public class GameScreenActivity extends AppCompatActivity {
 
     private Button m_btnEnter;
     private EditText m_etAnswer;
-    private TextView m_tvScrambledWord;
     private Controller m_controller;
     private String m_result;
+    TextView m_tvFirstLetter;
+    TextView m_tvSecondLetter;
+    TextView m_tvThirdLetter;
+    TextView m_tvFourthLetter;
+    TextView m_tvFifthLetter;
+    TextView m_tvSixthLetter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
 
-        m_tvScrambledWord = (TextView)findViewById(R.id.tvScrambledWord);
         m_controller = new Controller();
 
         m_btnEnter = (Button)findViewById(R.id.btnEnter);
@@ -42,13 +48,25 @@ public class GameScreenActivity extends AppCompatActivity {
         m_etAnswer = (EditText)findViewById(R.id.etAnswer);
         m_etAnswer.addTextChangedListener(this.watcher);
 
+        m_tvFirstLetter = (TextView)findViewById(R.id.tvFirstLetter);
+        m_tvSecondLetter = (TextView)findViewById(R.id.tvSecondLetter);
+        m_tvThirdLetter = (TextView)findViewById(R.id.tvThirdLetter);
+        m_tvFourthLetter = (TextView)findViewById(R.id.tvFourthLetter);
+        m_tvFifthLetter = (TextView)findViewById(R.id.tvFifthLetter);
+        m_tvSixthLetter = null;
+
         Intent intent = getIntent();
         int letterCount = intent.getIntExtra(MainActivity.LETTER_COUNT, 0);
+        String theWord = "";
         if (letterCount == 5) {
-            m_tvScrambledWord.setText(m_controller.getAFiveCharWord());
+            theWord = m_controller.getAFiveCharWord();
+            m_controller.setTheWord(theWord);
+            setLettersInTextViews();
         }
         else if (letterCount == 6) {
-            m_tvScrambledWord.setText(m_controller.getASixCharWord());
+            theWord = m_controller.getASixCharWord();
+            addSixthLetterTextView();
+            setLettersInTextViews();
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -63,6 +81,29 @@ public class GameScreenActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void addSixthLetterTextView() {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.llLetters);
+        m_tvSixthLetter = new TextView(this);
+        m_tvSixthLetter.setId(R.id.tvSixthLetter);
+        m_tvSixthLetter.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        m_tvSixthLetter.setGravity(Gravity.CENTER);
+        m_tvSixthLetter.setTextColor(Color.WHITE);
+        m_tvSixthLetter.setTextSize(30);
+        m_tvSixthLetter.setBackgroundResource(R.drawable.colored_box);
+        linearLayout.addView(m_tvSixthLetter);
+    }
+
+    private void setLettersInTextViews() {
+        m_tvFirstLetter.setText(m_controller.getLetterAtPosition(0));
+        m_tvSecondLetter.setText(m_controller.getLetterAtPosition(1));
+        m_tvThirdLetter.setText(m_controller.getLetterAtPosition(2));
+        m_tvFourthLetter.setText(m_controller.getLetterAtPosition(3));
+        m_tvFifthLetter.setText(m_controller.getLetterAtPosition(4));
+        if (m_tvSixthLetter != null) {
+            m_tvSixthLetter.setText(m_controller.getLetterAtPosition(5));
+        }
     }
 
     /**
