8bcfd654d53c68891beae2876eb6d75cf9b0b792
Betsy Louis <blouis1@my.westga.edu>
2016-03-23 10:44:54 -0400

    Refactored code on Main screen to use two buttons instead of radiobuttons. Added background to Main Screen.

--- app/src/androidTest/java/edu/westga/betsyjeffwordjumble/viewTests/MainActivityTests.java
+++ app/src/androidTest/java/edu/westga/betsyjeffwordjumble/viewTests/MainActivityTests.java
@@ -17,10 +17,8 @@ import edu.westga.betsyjeffwordjumble.view.MainActivity;
 public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
 
     MainActivity m_activity;
-    private Button m_btnPlay;
-    private RadioGroup m_radioGroup;
-    private RadioButton m_rb_five_letter_word;
-    private RadioButton m_rb_six_letter_word;
+    private Button m_btnFiveLetter;
+    private Button m_btnSixLetter;
 
     public MainActivityTests() {
         super(MainActivity.class);
@@ -29,59 +27,40 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
     @Override
     protected void setUp() {
         m_activity = getActivity();
-        m_btnPlay = (Button) m_activity.findViewById(R.id.btnPlay);
-        m_radioGroup = (RadioGroup) m_activity.findViewById(R.id.radioGroup);
-        m_rb_five_letter_word = (RadioButton) m_activity.findViewById(R.id.five_letters);
-        m_rb_six_letter_word = (RadioButton) m_activity.findViewById(R.id.six_letters);
+        m_btnFiveLetter = (Button) m_activity.findViewById(R.id.btnFiveLetterGame);
+        m_btnSixLetter = (Button) m_activity.findViewById(R.id.btnSixLetterGame);
     }
 
     public void testActivityExists() {
         assertNotNull(m_activity);
     }
 
-    public void testPlayButtonIsDisabled() {
-        m_radioGroup.clearCheck();
-        assertFalse("Play blue_button is not disabled.", m_btnPlay.isEnabled());
+    public void testFiveLetterButtonIsEnabled() {
+        assertTrue("Five letter game button is not enabled.", m_btnFiveLetter.isEnabled());
     }
 
-    public void selectRadioButton1() {
-        getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                //MainActivityTests.this.nameEditText.requestFocus();
-                MainActivityTests.this.m_rb_five_letter_word.setChecked(true);
-            }
-        });
+    public void testSixLetterButtonIsEnabled() {
+        assertTrue("Six letter game button is not enabled.", m_btnSixLetter.isEnabled());
     }
 
-    public void selectRadioButton2 () {
-        getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                //MainActivityTests.this.nameEditText.requestFocus();
-                MainActivityTests.this.m_rb_six_letter_word.setChecked(true);
-            }
-        });
-    }
-
-    public void testPlayButtonIsEnabledWhenFirstRadioButtonIsSelected() {
-
-        selectRadioButton1();
-        assertTrue("Play blue_button is not enabled.", m_btnPlay.isEnabled());
-    }
-
-    public void testPlayButtonIsEnabledWhenSecondRadioButtonIsSelected() {
-        selectRadioButton2();
-        assertTrue("Play blue_button is not enabled.", m_btnPlay.isEnabled());
-    }
-
-    public void testPlayButtonLoadsGameScreenActivity() {
+    public void testFiveLetterButtonLoadsGameScreenActivity() {
         // register next activity that need to be monitored.
         Instrumentation.ActivityMonitor activityMonitor = getInstrumentation()
                 .addMonitor(GameScreenActivity.class.getName(), null, false);
-        selectRadioButton1();
-        // Tap play blue_button
-        TouchUtils.clickView(this, m_btnPlay);
+        // Tap five letter button
+        TouchUtils.clickView(this, m_btnFiveLetter);
+        GameScreenActivity nextActivity = (GameScreenActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+        // next activity is opened and captured.
+        assertNotNull("Game screen activity is not launched", nextActivity);
+        nextActivity .finish();
+    }
+
+    public void testSixLetterButtonLoadsGameScreenActivity() {
+        // register next activity that need to be monitored.
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation()
+                .addMonitor(GameScreenActivity.class.getName(), null, false);
+        // Tap six letter button
+        TouchUtils.clickView(this, m_btnSixLetter);
         GameScreenActivity nextActivity = (GameScreenActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
         // next activity is opened and captured.
         assertNotNull("Game screen activity is not launched", nextActivity);
--- app/src/main/java/edu/westga/betsyjeffwordjumble/view/MainActivity.java
+++ app/src/main/java/edu/westga/betsyjeffwordjumble/view/MainActivity.java
@@ -2,10 +2,7 @@ package edu.westga.betsyjeffwordjumble.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,31 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     public final static String LETTER_COUNT = "edu.westga.betsyjeffwordjumble.LETTER_COUNT";
 
-    private RadioGroup m_radioGroup;
-    private Button m_btnPlay;
-    private RadioButton m_checkedRadioButton;
+    private Button m_btnFiveLetter;
+    private Button m_btnSixLetter;
+    private int m_letterCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        m_btnPlay = (Button)findViewById(R.id.btnPlay);
-        m_btnPlay.setEnabled(false);
-        m_radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        m_radioGroup.clearCheck();
-        m_radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                if (m_radioGroup.getCheckedRadioButtonId() == -1) {
-                    m_btnPlay.setEnabled(false);
-                } else {
-                    m_btnPlay.setEnabled(true);
-                    m_checkedRadioButton = (RadioButton) m_radioGroup.findViewById(m_radioGroup.getCheckedRadioButtonId());
-                }
-            }
-        });
+        m_btnFiveLetter = (Button)findViewById(R.id.btnFiveLetterGame);
+        m_btnSixLetter = (Button)findViewById(R.id.btnSixLetterGame);
     }
 
     @Override
@@ -68,22 +51,22 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the Send blue_button */
-    public void playGame(View view) {
+    /** Called when the user clicks the five letter challenge button */
+    public void playFiveLetterGame(View view) {
+        m_letterCount = 5;
+        playGame();
+    }
+
+    /** Called when the user clicks the six letter challenge button */
+    public void playSixLetterGame(View view) {
+        m_letterCount = 6;
+        playGame();
+    }
+
+    public void playGame(){
         Intent intent = new Intent(this, GameScreenActivity.class);
-        int checkedIndex = m_radioGroup.indexOfChild(m_checkedRadioButton);
-        int numberOfLetters = 0;
-        switch (checkedIndex) {
-            case 0:
-                numberOfLetters = 5;
-                break;
-            case 1:
-                numberOfLetters = 6;
-                break;
-            default:
-                break;
-        }
-        intent.putExtra(LETTER_COUNT, numberOfLetters);
+
+        intent.putExtra(LETTER_COUNT, m_letterCount);
         startActivity(intent);
     }
 }
